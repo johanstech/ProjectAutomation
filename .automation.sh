124067d13d2868e84b83b108942b63814caa04a6
Johan Andersson <johanstechs@gmail.com>
2021-03-01 06:15:13 +0100

    Automation code for project initialisation

---
+++ .automation.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+function create() {
+    cd
+    source ~/Projects/ProjectAutomation/.env
+    remoteGit $1
+    mkdir $PROJECT_FILEPATH$1
+    cd $PROJECT_FILEPATH$1
+    localGit $1
+}
+
+function remoteGit() {
+    curl \
+    -X POST \
+    -H 'Accept: application/vnd.github.v3+json' \
+    -H 'Authorization: token '$GIT_ACCESS_TOKEN \
+    https://api.github.com/user/repos \
+    -d '{"name":"'$1'"}'
+}
+
+function localGit() {
+    git init
+    git remote add origin https://github.com/$GIT_USERNAME/$1.git
+    touch README.md
+    touch .gitignore
+    git add .
+    git commit -m 'Initial commit'
+    git push -u origin master
+    code .
+}
